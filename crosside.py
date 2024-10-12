50d813c9b795884a2945eb55ebd544ba0b50ace7
atoya200 <agustintoya2004@gmail.com>
2024-10-12 17:19:51 -0300

    Comntarios actualizados

--- crosside.py
+++ crosside.py
@@ -1,18 +1,21 @@
 import requests
 
-# Hacemos una request y le colocamos la inyeccion
+# Hacemos una request y le colocamos la inyeccion en el param query
 gr = requests.get("http://localhost:8080/AltoroJ/search.jsp?query=<script>alert('Ataque XSS')</script>")
 
-# Recuperamos el texto y deberia de contener el texto que pasamos
+# Recuperamos el texto de la respuesta
 HTMLText = gr.text
 
-success = "<script>alert('Ataque XSS')</script>" in HTMLText
-
-if(success):
+# Comprobamos si el texto que nos devolvió Altoro con nuestra injección
+if("<script>alert('Ataque XSS')</script>" in HTMLText):
+	
+	# No se sanitizo la entrada, por lo cual nuestra injección fue un exito
 	print("Paso el ataque")
 	exit(1)
 
 else:
+	
+	# La entrada estaba sanitizada y por ende no fue exitoso nuestro ataque
 	print("Fallo el ataque")
 	exit(0)
 
